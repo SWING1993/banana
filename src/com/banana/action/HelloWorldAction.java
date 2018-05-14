5913474e754e71aa246403eb36a90bddb5b111ac
宋国华 <songguohua10446627@gmail.com>
2018-05-14 14:23:01 +0800

    update

---
+++ src/com/banana/action/HelloWorldAction.java
@@ -0,0 +1,10 @@
+package com.banana.action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+    @Override
+    public String execute() throws Exception {
+        System.out.println("’执行Action");
+        return SUCCESS;
+    }
+}
