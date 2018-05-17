3be7c85bf299c39d9794407cd0cf3ab5e28f9781
PC <songguohua10446627@gmail.com>
2018-05-17 12:41:55 +0800

    update

--- src/com/spring/annotation/TestObject.java
+++
@@ -1,8 +0,0 @@
-package com.spring.annotation;
-
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class TestObject {
-}
--- src/com/spring/annotation/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.annotation.repository;
-
-
-public interface UserRepository {
-
-    public void save();
-}
